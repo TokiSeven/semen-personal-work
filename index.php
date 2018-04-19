0066ba936322030e45e0cf49175f753fa559f838
Farera <227887@mail.ru>
2018-04-19 16:41:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php include('template-top.php'); ?>
-<div class="alert alert-success">Body</div>
+    <div class="jumbotron">
+        <h1 class="display-4">Здравствуйте дамы и господа!</h1>
+        <p class="lead">Сегодня я хотел бы продемонстрировать Вам мой сайт по деталям...</p>
+        <hr class="my-4">
+        <p>Предлагаю Вам пройти к просмотру классификаций деталей.</p>
+    <a class="btn btn-primary btn-lg" href="/list.php" role="button">Перейти</a>
+    </div>
 <?php include('template-bottom.php'); ?>
