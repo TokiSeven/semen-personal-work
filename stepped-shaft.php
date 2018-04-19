9e636e9e6e106eb1658ceb895521a2ef4de9102e
Владимир Рябцев (TokiSeven - MacPro) <tokiseven3@gmail.com>
2018-04-19 17:22:10 +0300

    Update stepped-shaft page (more designed)

--- stepped-shaft.php
+++ stepped-shaft.php
@@ -1,14 +1,42 @@
 <?php include('template-top.php'); ?>
-    <div>
-        <center>
-            <p class="lead">что-то о штамповке</p>
-            <div class="col-md-6">
-                <a class="btn btn-primary" href="technology-process.php">Перейти</a>
+
+<div class="jumbotron">
+    <h1 class="display-4">Ступенчатый вал</h1>
+    <p class="lead">Способы изготовления</p>
+    <hr class="my-4">
+    <p>калдптудклподук прук шопукр пжуокр пжукрпз угкрпз укрп</p>
+    <a class="btn btn-primary" href="technology-process.php">Перейти</a>
+    <button class="btn btn-primary" data-toggle="modal" data-target="#details">Подробнее</button>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="details" tabindex="-1" role="dialog" aria-labelledby="detailsLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="detailsLabel">Подробная информация</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="col-md-6">
-            
+            <div class="modal-body">
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
+                бла бла блабла бла блабла бла блабла бла блабла бла блабла бла блабла бла
             </div>
-        </center>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+            </div>
+        </div>
     </div>
+</div>
 
 <?php include('template-bottom.php'); ?>
