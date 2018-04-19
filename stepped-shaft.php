414ebdb5aa26668f4cc81756709bdbd0e001b877
Farera <227887@mail.ru>
2018-04-19 17:11:44 +0300

    Add stepped-shaft.php

---
+++ stepped-shaft.php
@@ -0,0 +1,14 @@
+<?php include('template-top.php'); ?>
+    <div>
+        <center>
+            <p class="lead">что-то о штамповке</p>
+            <div class="col-md-6">
+                <a class="btn btn-primary" href="technology-process.php">Перейти</a>
+            </div>
+            <div class="col-md-6">
+            
+            </div>
+        </center>
+    </div>
+
+<?php include('template-bottom.php'); ?>
